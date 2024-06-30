8ff1373b751535502e65ac93c27f7277258f24b0
Akansha <akanshav325@gmail.com>
2024-06-30 17:16:48 +0530

    Chapter-2

--- React_2/index.js
+++ React_2/index.js
@@ -1,9 +1,12 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 
 const head1 = React.createElement("h1", { id: "head1" }, "I am h1 tag");
+
 const head2 = React.createElement("h2", { id: "head2" }, "I am h2 tag");
+
 const child = React.createElement("div", { id: "child" }, [head1, head2]);
+
 const parent = React.createElement("div", { id: "parent" }, child);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
