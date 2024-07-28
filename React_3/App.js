978aad6b4be257ca7fbf50f7a1c8339afb11de8f
Akansha <akanshav325@gmail.com>
2024-07-29 00:18:48 +0530

    Chapter 3

--- React_3/App.js
+++ React_3/App.js
@@ -7,12 +7,26 @@ const heading = React.createElement("h1", { id: "head1" }, "I am h1 tag");
 
 
 //JSX ==> HTML-like or XML-like syntax
-//    ==> transpile before it reaches the JS engine ==> it is done by PARCEL
+//    ==>Babel transpiles it to React.createElemnt before it reaches the JS engine ==> ReactElement-JS Object ==> HTMLElement(render)
+//React Element
 const jsxHeading = <h1 id="head1">Namaste React</h1>
 
+//React Funtional Component
 
+const Title = () => (
+    <h1>Namaste React using JSX.</h1>
+)
+const HeadingCompo = () => (
+  <div className="heading">
+    <Title/>
+    <h1 className="head">Namaste React Funtional Component</h1>
+  </div>
+);
+ 
 //BOth are same things if you log heading or jsxHeading but writting in first method is not developer friendly
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(jsxHeading);
+// root.render(jsxHeading);
+root.render(<HeadingCompo/>);
 
 //Now we will not use React.createElement()
+
